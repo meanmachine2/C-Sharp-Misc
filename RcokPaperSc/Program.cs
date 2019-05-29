8c22fb8536780fb572cee2c708c0be22b2b9e1ef
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-05-30 00:48:22 +0200

    commit

--- RcokPaperSc/Program.cs
+++ RcokPaperSc/Program.cs
@@ -11,19 +11,19 @@ namespace RcokPaperSc
             string[] balance = new string[3] { "scissors", "rock", "paper" };
             Console.WriteLine("Enter numbers scissor (0), rock (1), paper (2): ");
             int unos = int.Parse(Console.ReadLine());           
-            int racunalo = 0, covjek = 0;          
+            int computer = 0, player = 0;          
             if (unos == compUnos) Console.WriteLine("Equal entry");
             else if ((unos==2&&compUnos==1)||(unos==1&&compUnos==0)||(unos==0&&compUnos==2))
             {
                 Console.WriteLine("{0} beats {1}", balance[unos], balance[compUnos]);
-                covjek++;
+                player++;
             }
             else
             {
                 Console.WriteLine("{0} beats {1}", balance[compUnos], balance[unos]);
-                racunalo++;
+                computer++;
             }
-            if (racunalo >= 1) Console.Write("Computer Wins");
+            if (computer >= 1) Console.Write("Computer Wins");
             else Console.Write("Player Wins");
         }
     }
