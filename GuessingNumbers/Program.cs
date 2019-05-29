4ebedeb0052f9cdfa78ca93bbe10cd0556964011
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-05-30 00:39:27 +0200

    commit

---
+++ GuessingNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GuessingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess random number from 0 to 100 ");
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            Random rand = new Random();
+            int randomNumber = rand.Next(0, 100);
+            while (number != randomNumber)
+            {
+                if (number>randomNumber)
+                {
+                    Console.Write("Number is too high, please enter the number again: ");
+                    number = int.Parse(Console.ReadLine());
+                }
+                else if (number<randomNumber)
+                {
+                    Console.Write("Number is too low, please enter the number again: ");
+                    number = int.Parse(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("The number is correct!");
+        }
+    }
+}
