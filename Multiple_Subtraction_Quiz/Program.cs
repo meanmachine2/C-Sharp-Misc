58d07db29cac627c485f091ab7a3cba62a13fef3
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-05-30 00:46:19 +0200

    commit

---
+++ Multiple_Subtraction_Quiz/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiple_Subtraction_Quiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- RcokPaperSc/Program.cs
+++ RcokPaperSc/Program.cs
@@ -8,12 +8,11 @@ namespace RcokPaperSc
         {
             Random rend = new Random();
             int compUnos = rend.Next(0, 2);
-            Console.WriteLine("racunalo je odabralo " + compUnos);
             string[] balance = new string[3] { "scissors", "rock", "paper" };
-            Console.WriteLine("Unesite scissor (0), rock (1), paper (2): ");
+            Console.WriteLine("Enter numbers scissor (0), rock (1), paper (2): ");
             int unos = int.Parse(Console.ReadLine());           
             int racunalo = 0, covjek = 0;          
-            if (unos == compUnos) Console.WriteLine("Jednako");
+            if (unos == compUnos) Console.WriteLine("Equal entry");
             else if ((unos==2&&compUnos==1)||(unos==1&&compUnos==0)||(unos==0&&compUnos==2))
             {
                 Console.WriteLine("{0} beats {1}", balance[unos], balance[compUnos]);
@@ -24,8 +23,8 @@ namespace RcokPaperSc
                 Console.WriteLine("{0} beats {1}", balance[compUnos], balance[unos]);
                 racunalo++;
             }
-            if (racunalo >= 1) Console.Write("Racunalo pobjedjuje");
-            else Console.Write("Covjek pobjedjuje");
+            if (racunalo >= 1) Console.Write("Computer Wins");
+            else Console.Write("Player Wins");
         }
     }
 }
