14ff6b756ab3c6495ddfdad1c07be03b2e322e9c
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-05-30 01:22:23 +0200

    commit

--- Multiple_Subtraction_Quiz/Program.cs
+++ Multiple_Subtraction_Quiz/Program.cs
@@ -6,7 +6,45 @@ namespace Multiple_Subtraction_Quiz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string [] answers = new string[5];
+            Random rand = new Random();
+            int timeStart = DateTime.Now.Second;
+            Console.WriteLine("You are presented with five math problems");
+            int correctAnswer = 0;         
+            for (int i = 0; i < answers.Length; i++)
+            {
+                int a = rand.Next(0, 10);
+                int b = rand.Next(0, 10);
+                int temp = 0;
+                if (a<b)
+                {
+                    temp = a;
+                    a = b;
+                    b = temp;
+                }
+                int answer = a - b;
+                Console.Write("{0} - {1} = ", a, b);
+                int c = int.Parse(Console.ReadLine());
+                if (c == answer)
+                {
+                    answers[i] = a + " - " + b + " = " + c + " - Correct!";
+                    Console.WriteLine("Correct!");
+                    correctAnswer++;
+                }
+                else
+                {
+                    Console.Write("Wrong, answer should be {0} - {1} = {2}", a, b, answer);
+                    answers[i] = a + " - " + b + " = " + c + " - Wrong!";
+                    Console.WriteLine();
+                }
+            }
+            int timeStop = (int)(DateTime.Now.Second) - timeStart;
+            Console.WriteLine("Test time is {0} seconds",timeStop);
+            Console.WriteLine("Correct count is " + correctAnswer);
+            for (int i = 0; i < answers.Length; i++)
+            {
+                Console.WriteLine(answers[i]);
+            }
         }
     }
 }
