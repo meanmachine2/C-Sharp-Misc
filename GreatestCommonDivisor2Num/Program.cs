5c33a318d303eff8792fbda5522fea188e39a67e
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-05-31 22:51:35 +0200

    commit

---
+++ GreatestCommonDivisor2Num/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace GreatestCommonDivisor2Num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number 1: ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter number 2: ");
+            int number2 = int.Parse(Console.ReadLine());
+            if (number2>number1)
+            {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+            int commonDivisor = number2;
+            bool greatestDivisor = false;
+            while (commonDivisor > 0 && greatestDivisor == false)
+            {
+                if (number1 % commonDivisor == 0 && number2 % commonDivisor == 0) greatestDivisor = true;
+                else commonDivisor--;
+            }
+            Console.WriteLine("Greatest divisor of two numbers is " + commonDivisor);
+        }
+    }
+}
