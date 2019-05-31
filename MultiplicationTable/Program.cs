a2a5cbf08d14ee9b121ff5fe6eba9bb69c916883
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-05-31 22:28:58 +0200

    commit

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("  ");
+            for (int i = 1; i < 10; i++) Console.Write(String.Format("{0,5}", i));
+            Console.WriteLine("\n ----------------------------------------------");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write(i+" ");
+                for (int j = 1; j < 10; j++) Console.Write(String.Format("{0,5}", i * j));
+                Console.WriteLine();
+            }
+        }
+    }
+}
