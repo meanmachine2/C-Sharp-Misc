49c2357e3fdf47ce4427a40c93cf5238cc61541a
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-02 16:05:54 +0200

    commit

---
+++ TwoHighestScores/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoHighestScores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
