ec65d0507134310aaf76a127cc2072e8f7aa2914
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-02 17:12:54 +0200

    commit

--- CheckNumber/Program.cs
+++ CheckNumber/Program.cs
@@ -8,7 +8,6 @@ namespace CheckNumber
         {
             Console.WriteLine("Enter number : ");
             int broj = int.Parse(Console.ReadLine());
-
             if (broj % 5 == 0 && broj % 6 == 0) Console.WriteLine("Number is divisible by 5 and 6");
             else if (broj % 5 == 0 ^ broj % 6 == 0) Console.WriteLine("Number is divisible by 5 or 6");
             else Console.WriteLine("Number is not divisible by 5 or 6");
