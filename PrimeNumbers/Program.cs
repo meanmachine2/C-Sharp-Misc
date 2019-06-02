8ce4dd02863b7d4f1a4695edf3a89fc47e33c8f8
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-02 13:54:15 +0200

    commit PrimeNumbers

---
+++ PrimeNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PrimeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prime = 0;
+            int i = 1;
+            while(prime<=50)
+            {
+                bool isPrimeNumber = true;
+                for (int j = i / 2; j >= 2; j--)
+                {
+                    if (i % j == 0) isPrimeNumber = false;
+                }
+                if (isPrimeNumber == true)
+                {
+                    if (prime % 5 == 0) Console.WriteLine();
+                    Console.Write(String.Format("{0,5}",i));
+                    prime++;
+                }
+                i++;
+            }
+        }
+    }
+}
