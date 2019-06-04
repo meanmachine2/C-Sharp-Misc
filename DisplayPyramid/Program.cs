7a0aca8e1cd949b56fa6f104c3cde2d97aa82e37
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-04 20:32:21 +0200

    commit

---
+++ DisplayPyramid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DisplayPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int space = (number * 2)+2;
+            for (int i = 1; i <= number; i++)
+            {
+                for (int z = 0; z < space; z++) Console.Write(" ");
+                for (int j = i; j >= 1; j--) Console.Write(j+" ");
+                if (i!=1)
+                {
+                    for (int k = 2; k <= i; k++) Console.Write(k + " ");
+                }
+                Console.WriteLine();
+                space -= 2;
+            }
+        }
+    }
+}
