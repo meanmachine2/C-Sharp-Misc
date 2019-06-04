5f364f2205a26176e5323665ff773d1a45c23fff
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-04 20:06:57 +0200

    commit

---
+++ FactorsOfInteger/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FactorsOfInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter integer");
+            int number = int.Parse(Console.ReadLine());
+            int factor = 2;
+            while (number>1)
+            {
+                if (number % factor == 0)
+                {
+                    Console.Write("{0} ", factor);
+                    number /= factor;
+                }
+                else factor++;
+            }
+        }
+    }
+}
---
+++ FactorsOfInteger/Readme.md
@@ -0,0 +1,2 @@
+﻿Write a program that reads an integer and displays all its smallest factors in increasing order. For example, if the input integer is 120,
+the output should be as follows: 2, 2, 2, 3, 5
