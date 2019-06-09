f216b257c99ab930ef6dec2680f918a3a6528599
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-09 10:35:16 +0200

    commit

---
+++ PrimeNumbersBetwen2and1000/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PrimeNumbersBetwen2and1000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int prime = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                bool isPrimeNumber = true;
+                for (int j = i / 2; j >= 2; j--)
+                {
+                    if (i % j == 0) isPrimeNumber = false;
+                }
+                if (isPrimeNumber == true)
+                {
+                    if (prime % 8 == 0) Console.WriteLine();
+                    Console.Write(String.Format("{0,5}", i));
+                    prime++;
+                }
+            }
+        }
+    }
+}
---
+++ PrimeNumbersBetwen2and1000/Readme.md
@@ -0,0 +1,3 @@
+﻿(Display prime numbers between 2 and 1,000)  Display all
+the prime numbers between 2 and 1,000, inclusive. Display eight prime numbers
+per line. Numbers are separated by exactly one space.
