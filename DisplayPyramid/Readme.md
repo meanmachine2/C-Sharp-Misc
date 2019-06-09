93e04a0b5b5269b4455458b5ebdcf0fbcecf4e4f
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-09 09:58:32 +0200

    commit

--- DisplayPyramid/Readme.md
+++ DisplayPyramid/Readme.md
@@ -1,10 +1,2 @@
 ﻿(Display pyramid ) Write a program that prompts the user to enter an integer from
-1 to 15 and displays a pyramid, as shown in the following sample run:
-Enter the number of lines:
-			1
-		  2 1 2
-		3 2 1 2 3
-	  4 3 2 1 2 3 4
-	5 4 3 2 1 2 3 4 5
-  6 5 4 3 2 1 2 3 4 5 6
-7 6 5 4 3 2 1 2 3 4 5 6 7
+1 to 10 and displays a pyramid
---
+++ PyramidPatterns/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PyramidPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pattern A");
+            int number = 6;
+            for (int i = 1; i <=number; i++)
+            {
+                for (int j = 1; j <= i; j++) Console.Write(j + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pattern B");
+            for (int i = number; i > 0; i--)
+            {
+                for (int j = 1; j <= i; j++) Console.Write(j + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pattern C");
+            int repeat = number * 2;
+            for (int i = 1; i <= number; i++)
+            {
+                for (int z = 0; z < repeat; z++)Console.Write(" ");
+                for (int j = i; j >=1; j--) Console.Write(j + " ");
+                Console.WriteLine();
+                repeat -= 2;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Pattern D");
+            number = 6;
+            repeat = 0;
+            for (int i = number; i > 0; i--)
+            {
+                for (int z = 0; z < repeat; z++) Console.Write(" ");
+                for (int j = 1; j <= i; j++) Console.Write(j + " ");
+                Console.WriteLine();
+                repeat+=2;
+            }
+
+        }
+    }
+}
