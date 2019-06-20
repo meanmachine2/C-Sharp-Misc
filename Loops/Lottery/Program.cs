d1548f9f493f44d4ead39e302fb4213603269fd4
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-20 19:31:09 +0200

    commit

---
+++ Loops/Lottery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            int firstDigit = number.Next(1, 9);
+            int secondDigit;
+            do
+            {
+                secondDigit = number.Next(1, 9);
+            } while (firstDigit==secondDigit);
+            Console.WriteLine($"Lottery numbers are {firstDigit}{secondDigit}");
+        }
+    }
+}
