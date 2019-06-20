3cb68ce5af149dc99e710ff123811fac82704bab
meanmachine2 <50801112+meanmachine2@users.noreply.github.com>
2019-06-20 19:57:47 +0200

    commit

---
+++ Loops/PerfectNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 10000; i++)
+            {
+                int sum = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0) sum += j;
+                }
+                if (sum == i) Console.WriteLine("Perfect number is " + i);
+            }
+        }
+    }
+}
